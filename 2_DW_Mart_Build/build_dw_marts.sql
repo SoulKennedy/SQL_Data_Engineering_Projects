c3ca9f66f11c34c0f0a44a95c2a08c46db467a82
dataforge-io <ykaysings@gmail.com>
2026-08-18 20:51:18 +0100

    build and validate flat mart

--- 2_DW_Mart_Build/build_dw_marts.sql
+++ 2_DW_Mart_Build/build_dw_marts.sql
@@ -1,5 +1,9 @@
+-- duckdb dw_marts.duckdb -c ".read build_dw_marts.sql"
 -- Step 1: DW - Create star schema tables
 .read 01_create_tables_dw.sql
 
 -- Step 2: DW - Load DATA CSV files into tables
 .read 02_load_schema_dw.sql
+
+-- Step 3: Mart - Creat flat mart
+.read 03_create_flat_mart.sql
